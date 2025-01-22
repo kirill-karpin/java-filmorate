5b4dc3dd72ea9879c44ef37e8c573358e664bef5
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-01-22 13:11:43 +0500

    feature: persist layer

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -39,4 +39,6 @@ public class UserController {
   public Collection<User> listUser() {
     return userRepository.getAll();
   }
+
+
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InMemoryUserStorage {
+
+}
